dee612993dec5b4012bf3750e281070dfd3bb0a9
jasons1425 <jasonns1425@gmail.com>
2022-03-02 21:57:20 +0800

    Refine user model schema, remove redundant field

--- backend/user_auth/models.py
+++ backend/user_auth/models.py
@@ -1,11 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from django.core.exceptions import ValidationError
 import os
 
 
 def get_avatar_fp(instance, filename):
     return os.path.join("avatar/", f"{instance.username}_") + filename
+
+
+class CrowdUser(AbstractUser):
+    is_org = models.BooleanField(default=False, null=False, blank=False)
+    avatar = models.ImageField(upload_to=get_avatar_fp, null=True, blank=True)
+    gender = models.CharField(
+        max_length=2,
+        choices=[
+            ('M', "male"),
+            ('F', "female"),
+            ("NA", "others"),
+        ],
+        default="NA",
+        null=False,
+        blank=False
+    )
+    date_of_birth = models.DateField(auto_now_add=True, null=False, blank=False)
+
+
+def validate_sid(value):
+    if not isinstance(value, str):
+        raise ValidationError("Expect SID to be in string type.")
+    if not(len(value) == 10 and value.isdigit()):
+        raise ValidationError("SID should consist of exactly 10 digits.")
+    return True
 
 
 class StudentProfile(models.Model):
@@ -15,6 +41,8 @@ class StudentProfile(models.Model):
                                 null=False, blank=False)
 
     # student user related
+    sid = models.CharField(max_length=10, validators=[validate_sid],
+                           null=False, blank=False)
     major = models.CharField(max_length=50,
                              null=False, blank=False)
     admission_year = models.DateField(null=False, blank=False)
@@ -38,20 +66,4 @@ class OrgUserProfile(models.Model):
         return self.user.username
 
 
-class CrowdUser(AbstractUser):
-    is_org = models.BooleanField(default=False, null=False, blank=False)
-    avatar = models.ImageField(upload_to=get_avatar_fp, null=True, blank=True)
-    gender = models.CharField(
-        max_length=2,
-        choices=[
-            ('M', "male"),
-            ('F', "female"),
-            ("NA", "others"),
-        ],
-        default="NA",
-        null=False,
-        blank=False
-    )
-    date_of_birth = models.DateField(auto_now_add=True, null=False, blank=False)
-    create_time = models.DateTimeField(auto_now_add=True, editable=False)
 
