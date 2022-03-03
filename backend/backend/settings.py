5cf88e2e35695d1cc216a0b5eecbd9b35e3c2164
cheungkw0 <1155142376@link.cuhk.edu.hk>
2022-03-03 23:01:05 +0800

    Update sprint 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -152,6 +152,8 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
---
+++ frontend/src/pages/Quest.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Quest() {
+  return (
+    <div>Quest</div>
+  )
+}
+
+export default Quest
