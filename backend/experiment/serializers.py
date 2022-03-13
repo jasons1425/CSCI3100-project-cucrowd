2ca926a16a78d996ba24b750556294a291f42beb
jasons1425 <jasonns1425@gmail.com>
2022-03-13 13:34:43 +0800

    Allow creating new experiment objects after login

--- backend/experiment/serializers.py
+++ backend/experiment/serializers.py
@@ -3,6 +3,13 @@ from .models import Experiment
 
 
 class ExperimentSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        if self.context['host']:
+            host = self.context['host']
+            validated_data['host'] = host
+        experiment = Experiment.objects.create(**validated_data)
+        return experiment
+
     class Meta:
         model = Experiment
-        fields = ("id", "host", "title", "description")
+        fields = "__all__"
--- backend/experiment/views.py
+++ backend/experiment/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import ExperimentSerializer
 from .models import Experiment
@@ -19,6 +20,19 @@ class ExperimentView(viewsets.ModelViewSet):
         'update': [IsAuthenticated],
         'partial_update': [IsAuthenticated],
     }
+
+    # referencing the method from the below website
+    # https://ilovedjango.com/django/rest-api-framework/tips/save-foreign-key-using-django-rest-framework-create-method/
+    # if the "host" field info is passed to the serializer via the data argument,
+    #    that field will disappear when reaching the serializer.create() method due to unknown reasons
+    def create(self, request, *args, **kwargs):
+        host = request.user
+        _serializer = self.serializer_class(data=request.data, context={"host": host})
+        if _serializer.is_valid(raise_exception=True):
+            self.perform_create(_serializer)
+            return Response(data=_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get_permissions(self):
         try:
