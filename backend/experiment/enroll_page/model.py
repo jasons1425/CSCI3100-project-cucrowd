29bc07835004f3326906cecced298abec942f78c
cychoi0 <79830983+cychoi0@users.noreply.github.com>
2022-03-17 21:47:47 +0800

    Update model.py

--- backend/experiment/enroll_page/model.py
+++ backend/experiment/enroll_page/model.py
@@ -9,6 +9,6 @@ class Enroll_field(models.Model):
     employer=models.ForeignKey(OrgUserProfile.org_name,null=False, blank=False,editable=False)
     highlight=models.ForeignKey(Experiment_field.highlight,null=False, blank=False,editable=False)
     target=models.ForeignKey(Experiment_field.target,null=False, blank=False,editable=False)
-    timeslot_LIST = models.ForeignKey(Experiment_field.time_list,null=False, blank=False,editable=False)
-    time_slot=models.IntegerField(choices=timeslot_LIST)
+    #timeslot_LIST = models.ForeignKey(Experiment_field.time_list,null=False, blank=False,editable=False)
+    #time_slot=models.IntegerField(choices=timeslot_LIST)
     vacancy = models.ForeignKey(Experiment_field.vacancy,null=False, blank=False,editable=False)
