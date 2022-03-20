0ef4153089e2212da59c8d762379f712ac21ba80
jasons1425 <jasonns1425@gmail.com>
2022-03-20 19:29:27 +0800

    Add available time slots constraint to experiment models

--- backend/experiment/serializers.py
+++ backend/experiment/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Experiment
+from .models import Experiment, Enrollment
 from user_auth.serializers import CrowdUserSerializer
 
 
@@ -7,7 +7,7 @@ class ExperimentSerializer(serializers.ModelSerializer):
     host = CrowdUserSerializer(many=False, required=False, allow_null=True)
 
     def create(self, validated_data):
-        if self.context['host']:
+        if self.context.get("host", None):
             host = self.context['host']
             validated_data['host'] = host
         experiment = Experiment.objects.create(**validated_data)
@@ -19,3 +19,20 @@ class ExperimentSerializer(serializers.ModelSerializer):
                   "job_nature", "type", "duration", "salary", "venue",
                   "deadline", "vacancy", "description"]
 
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    participant = CrowdUserSerializer(many=False, required=False, allow_null=True)
+
+    def create(self, validated_data):
+        if self.context.get("participant", None):
+            participant = self.context['participant']
+            validated_data['participant'] = participant
+        if self.context.get("experiment", None):
+            exp_obj = self.context['experiment']
+            validated_data['experiment'] = exp_obj
+        enrollment = Enrollment.objects.create(**validated_data)
+        return enrollment
+
+    class Meta:
+        model = Enrollment
+        fields = ["id", "experiment", "participant", "selected_time"]
