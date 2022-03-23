f9eaacd8f9b2e0b9d3e70dbc821ecf8da181989d
cychoi0 <79830983+cychoi0@users.noreply.github.com>
2022-03-23 16:25:07 +0800

    Update admin.py

--- backend/teamformation/admin.py
+++ backend/teamformation/admin.py
@@ -1,1 +1,20 @@
+from django.contrib import admin
+from .models import Teammates, Teamformation
 
+# Register your models here.
+class TeammatesAdmin(admin.ModelAdmin):
+    list_display = ("info")
+    search_fields = ("info")
+
+
+
+
+class TeamformationAdmin(admin.ModelAdmin):
+    list_display = ("id",)
+    search_fields = ()
+
+
+
+
+admin.site.register(Teammates, TeammatesAdmin)
+admin.site.register(Teamformation, TeamformationAdmin)
