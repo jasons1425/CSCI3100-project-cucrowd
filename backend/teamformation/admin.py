4a57046c4d6620adfda3e15d184f47f4244fe0b3
cychoi0 <79830983+cychoi0@users.noreply.github.com>
2022-03-31 14:07:14 +0800

    Update admin.py

--- backend/teamformation/admin.py
+++ backend/teamformation/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError as FieldValidationError
 from .models import Teammates, Teamformation
 
 # Register your models here.
-class TeammatesAdmin(admin.ModelAdmin):
-    list_display = ("info",)
-
 
 class TeamformationAdmin(admin.ModelAdmin):
-    list_display = ("id",)
-    
+    list_display = ("id","title","host","teamsize","post_date",)
+
+
     def save_model(self, request, obj, form, change):
         if getattr(obj, 'host', None) is None:
             obj.host = request.user
         obj.save()
 
+class TeammatesAdmin(admin.ModelAdmin):
+    list_display = ("id","info","teamformation")
 
-admin.site.register(Teammates, TeammatesAdmin)
+
 admin.site.register(Teamformation, TeamformationAdmin)
+admin.site.register(Teammates, TeammatesAdmin)
