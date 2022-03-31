cd311eeaaa45148e96a384943dfe82b5e8a8a0fa
cychoi0 <79830983+cychoi0@users.noreply.github.com>
2022-03-31 14:11:37 +0800

    Create forms.py

---
+++ backend/teamformation/forms.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import ValidationError
+class Teamformation(forms.ModelForm):
+    class Meta:
+        model = Teamformation
+
+    def clean(self):
+        teammates = self.cleaned_data.get('teammates')
+        teammsize = self.cleaned_data.get('teammsize')
+        if teammates and teammates.count() > teammsize :
+            raise ValidationError('Oops! NO Vacancy is left.')
+        return self.cleaned_data
