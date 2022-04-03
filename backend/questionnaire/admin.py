89c86607eaf1e99a9770dea778b4c4af429f59f1
cychoi0 <79830983+cychoi0@users.noreply.github.com>
2022-04-03 15:55:50 +0800

    Add files via upload

---
+++ backend/questionnaire/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError as FieldValidationError
+from .models import Questionnaire, Answer
+
+
+# Register your models here.
+
+class QuestionnaireAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "host", "questionsize", "post_date", "publishable")
+
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'host', None) is None:
+            obj.host = request.user
+        obj.save()
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("id", "questionnaire", "Respondent")
+
+admin.site.register(Questionnaire, QuestionnaireAdmin)
+admin.site.register(Answer, AnswerAdmin)
