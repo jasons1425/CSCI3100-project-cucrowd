3aeca2e7856caee31292e3ae75bafd8c2b441f2d
cheungkw0 <79793713+cheungkw0@users.noreply.github.com>
2022-04-04 11:21:51 +0800

    Interface of questionnaire

---
+++ Questionnaire_add.js
@@ -0,0 +1,154 @@
+import './Questionnaire_add.css'
+import * as Ri from "react-icons/ri";
+import {useState} from 'react'
+
+function Questionnaire_add() {
+    const [type, setType] = useState(1);
+    const [number, setNumber] = useState(1);
+
+    function changeQuestionNumber(){
+        let x = document.getElementById("questionnaire_number").value;
+        if(x > 10){
+            setNumber(10);
+        }else{
+            setNumber(x);
+        }
+    }
+
+    function changeType(){
+        let x = document.getElementById("questionnaire_type").value;
+        if(x === "MC"){
+            setType(1);
+        }else if(x === "longQ"){
+            setType(2);
+        }else if(x === "scoring"){
+            setType(3);
+        }else if(x === "googleForm"){
+            setType(4);
+        }
+    }
+
+    return (
+      <div className="questionnaire_page">
+        <header className="questionnaire_header">
+            <div className="questionnaire_icon">
+            <Ri.RiNewspaperLine />
+            </div>
+            <div className="questionnaire_title">
+            Questionnaire
+            </div>
+        </header>
+
+        <div className="questionnaire_space">
+            &nbsp;
+        </div>
+        
+        
+        <section className="questionnaire_add">
+            <div className="questionnaire_add_title">
+                <label htmlFor="questionnaire_title">Title</label>
+                <input type="text" placeholder="Type your questionnaire title here" name="questionnaire_title" id="questionnaire_title"></input>
+            </div>
+
+            <div className="questionnaire_add_time">
+                <label htmlFor="questionnaire_deadline">Deadline</label>
+                <input type="date" name="questionnaire_deadline" id="questionnaire_deadline"></input>
+                <label htmlFor="questionnaire_eft">Expected Finishing Time</label>
+                <input type="text" placeholder="Expected finishing time" name="questionnaire_eft" id="questionnaire_eft"></input>
+            </div>
+
+            <div className="questionnaire_add_purpose">
+                <label htmlFor="questionnaire_purpose">Purpose / Description</label>
+                <textarea placeholder="Your purpose and description" name="questionnaire_purpose" id="questionnaire_purpose"></textarea>
+            </div>
+
+            <div className="questionnaire_add_type">
+                <label htmlFor="questionnaire_type">Question type</label>
+                <select name="questionnaire_type" id="questionnaire_type" onChange={()=>changeType()}>
+                    <option value="MC">MC</option>
+                    <option value="longQ">Long question</option>
+                    <option value="scoring">Scoring</option>
+                    <option value="googleForm">Google Form</option>
+                </select>
+                { type === 4 
+                    ? <></> 
+                    :   <>
+                            <label htmlFor="questionnaire_number">No. of Questions</label>
+                            <input id="questionnaire_number" name="questionnaire_number" type="number" min="1" max="10" value={number} onChange={changeQuestionNumber}></input>
+                        </>
+                }
+            </div>
+
+            <div>
+                {type === 1 && <MCQ number={number}/>}
+                {type === 2 && <Question number={number}/>}
+                {type === 3 && <Question number={number}/>}
+                {type === 4 && <Form />}    
+            </div>
+        </section>
+        <div>
+            <button className="questionnaire_edit_back_button" onClick={()=> window.location.pathname="/question/edit"}>Back</button>
+            <button className="questionnaire_edit_add_button" onClick={()=>addPost()}>Add New Post</button>
+        </div>
+      </div>
+    )
+}
+
+export default Questionnaire_add;
+
+function MCQ({number}){
+    var all_question = [];
+    for(var id = 1; id <= number; id++){
+        all_question.push(
+            <div className="MCQ_add">  
+                <div className="add_question">
+                    <label htmlFor={id}>Question {id}: </label>
+                    <input type="text" name={id} id={id}></input>
+                </div>
+                <div>
+                    <label htmlFor={id + "A"}>A: </label>
+                    <input type="text" name={id + "A"} id={id + "A"}></input>
+                </div>
+                <div>
+                    <label htmlFor={id + "B"}>B: </label>
+                    <input type="text" name={id + "B"} id={id + "B"}></input>
+                </div>
+                <div>
+                    <label htmlFor={id + "C"}>C: </label>
+                    <input type="text" name={id + "C"} id={id + "C"}></input>
+                </div>
+                <div>
+                    <label htmlFor={id + "D"}>D: </label>
+                    <input type="text" name={id + "D"} id={id + "D"}></input>
+                </div>    
+            </div>
+        );
+    }
+    return all_question
+}
+
+function Question({number}){
+    var all_question = [];
+    for(var id = 1; id <= number; id++){
+        all_question.push(
+            <div className="add_question">  
+                <label htmlFor={id}>Question {id}: </label>
+                <input type="text" name={id} id={id}></input> 
+            </div>
+        );
+    }
+    return all_question
+}
+
+function Form(){
+    return(
+        <div className="add_question">  
+            <label htmlFor="googleForm">Link</label>
+            <input type="text" name="googleForm" id="googleForm"></input> 
+        </div>
+    )
+}
+
+function addPost(){
+
+}
