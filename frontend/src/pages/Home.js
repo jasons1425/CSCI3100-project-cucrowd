e3611ebbda81727daea05fe40589734b4a7341cb
cheungkw0 <79793713+cheungkw0@users.noreply.github.com>
2022-04-08 11:12:54 +0800

    update home page team list

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,11 @@ import React, { Component, useState ,useEffect} from 'react'
 import sample from '../Global/sample.jpg'
 import './Home.css'
 import axios from "axios";
+import member5 from '../components/5-people.png'
+import member4 from '../components/4-people.png'
+import member3 from '../components/3-people.png'
+import member2 from '../components/2-people.png'
+
 function Home() {
   return (
     <div className='home'>
@@ -45,22 +50,7 @@ function Home() {
         <div className='title-sub'>Team Formation:</div>
         <div className='container'>
         <div className='row'>
-            <div className='column'>
-                <div className='postauthor'>postauthor</div>
-                <div className='postphoto'>photo</div>
-                <div className='description'>this is description</div>
-            </div>
-            <div className='column'>
-            <div className='postauthor'>postauthor</div>
-                <div className='postphoto'>photo</div>
-                <div className='description'>this is description</div>
-                
-            </div>
-            <div className='column'>
-                <div className='postauthor'>postauthor</div>
-                <div className='postphoto'>photo</div>
-                <div className='description'>this is description</div>
-            </div>    
+              <Team_list/>
             </div>
             <button className='viewmore' onClick={()=>{window.location.pathname='/team';}}>View More</button>
         </div>
@@ -71,12 +61,12 @@ function Home() {
 
 class Exp_list extends React.Component{
     constructor(props) {
-    super(props);
+        super(props);
 
-    this.state = {
-        items: [],
-    };
-}
+        this.state = {
+            items: [],
+        };
+    }
 
   componentDidMount() {
   fetch(
@@ -88,7 +78,6 @@ class Exp_list extends React.Component{
   }
   
   render(){
-    const {items } = this.state;
     try{
         //console.log(items[0].id);
      
@@ -96,31 +85,31 @@ class Exp_list extends React.Component{
       <div >     
      
             <>
-            <div className='column'>
-                <div className='postauthor'>{items[0].title.substring(0,200)}</div>
-                <div className='postphoto'><img src={items[0].exp_img} width="440px" height="390px" ></img></div>
+            {this.state.items.length > 0 && <div className='column'>
+                <div className='postauthor'>{this.state.items[0].title.substring(0,200)}</div>
+                <div className='postphoto'><img src={this.state.items[0].exp_img} width="440px" height="390px" ></img></div>
                 <div className='home_line'><hr id="line"/></div>
-                <div className='description'>{items[0].description}</div>
-            </div>
-            <div className='column'>
-                <div className='postauthor'>{items[1].title.substring(0,200)}</div>
-                <div className='postphoto'><img src={items[1].exp_img} width="440px" height="390px" ></img></div>
+                <div className='description'>{this.state.items[0].description}</div>
+            </div>}
+            {this.state.items.length > 1 && <div className='column'>
+                <div className='postauthor'>{this.state.items[1].title.substring(0,200)}</div>
+                <div className='postphoto'><img src={this.state.items[1].exp_img} width="440px" height="390px" ></img></div>
                 <div className='home_line'><hr id="line"/></div>
-                <div className='description'>{items[1].description}</div>
-            </div>
-            <div className='column'>
-                <div className='postauthor'>{items[2].title.substring(0,200)}</div>
-                <div className='postphoto'><img src={items[2].exp_img} width="440px" height="390px" ></img></div>
+                <div className='description'>{this.state.items[1].description}</div>
+            </div>}
+            {this.state.items.length > 2 && <div className='column'>
+                <div className='postauthor'>{this.state.items[2].title.substring(0,200)}</div>
+                <div className='postphoto'><img src={this.state.items[2].exp_img} width="440px" height="390px" ></img></div>
                 <div className='home_line'><hr id="line"/></div>
-                <div className='description'>{items[2].description}</div>
-            </div>
+                <div className='description'>{this.state.items[2].description}</div>
+            </div>}
             </>
          
       
   </div>
     );
     }catch(e){
-        return("")
+        return(""+e)
  }
   }
 }
@@ -145,3 +134,67 @@ function Name(){
       }
 
 export default Home
+
+
+class Team_list extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+        };
+    }
+
+  componentDidMount() {
+  fetch(
+      "http://localhost:8000/api/teamformation")
+      .then((res) => res.json())
+      .then((json) => {
+          this.setState({items: json,});
+      })
+  }
+  
+  render(){
+    try{
+        //console.log(items[0].id);
+     
+    return(
+      <div >     
+            <>
+            {this.state.items.length > 0 && <div className='column'>
+                <div className='postauthor'>{this.state.items[0].title.substring(0,200)}</div>
+                {this.state.items[0].teamsize == 2 && <div className='postphoto'><img src={member2} width="440px" height="390px" ></img></div>}
+                {this.state.items[0].teamsize == 3 && <div className='postphoto'><img src={member3} width="440px" height="390px" ></img></div>}
+                {this.state.items[0].teamsize == 4 && <div className='postphoto'><img src={member4} width="440px" height="390px" ></img></div>}
+                {this.state.items[0].teamsize == 5 && <div className='postphoto'><img src={member5} width="440px" height="390px" ></img></div>}
+                <div className='home_line'><hr id="line"/></div>
+                <div className='description'>{this.state.items[0].description}</div>
+            </div>}
+            {this.state.items.length > 1 && <div className='column'>
+                <div className='postauthor'>{this.state.items[1].title.substring(0,200)}</div>
+                {this.state.items[1].teamsize == 2 && <div className='postphoto'><img src={member2} width="440px" height="390px" ></img></div>}
+                {this.state.items[1].teamsize == 3 && <div className='postphoto'><img src={member3} width="440px" height="390px" ></img></div>}
+                {this.state.items[1].teamsize == 4 && <div className='postphoto'><img src={member4} width="440px" height="390px" ></img></div>}
+                {this.state.items[1].teamsize == 5 && <div className='postphoto'><img src={member5} width="440px" height="390px" ></img></div>}
+                <div className='home_line'><hr id="line"/></div>
+                <div className='description'>{this.state.items[1].description}</div>
+            </div>}
+            {this.state.items.length > 2 && <div className='column'>
+                <div className='postauthor'>{this.state.items[2].title.substring(0,200)}</div>
+                {this.state.items[2].teamsize == 2 && <div className='postphoto'><img src={member2} width="440px" height="390px" ></img></div>}
+                {this.state.items[2].teamsize == 3 && <div className='postphoto'><img src={member3} width="440px" height="390px" ></img></div>}
+                {this.state.items[2].teamsize == 4 && <div className='postphoto'><img src={member4} width="440px" height="390px" ></img></div>}
+                {this.state.items[2].teamsize == 5 && <div className='postphoto'><img src={member5} width="440px" height="390px" ></img></div>}
+                <div className='home_line'><hr id="line"/></div>
+                <div className='description'>{this.state.items[2].description}</div>
+            </div>}
+            </>
+         
+      
+  </div>
+    );
+    }catch(e){
+        return(""+e)
+ }
+  }
+}
