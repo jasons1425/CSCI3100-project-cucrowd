7337862f207e1ea084dc4658027bc7f299f9ec19
cheungkw0 <79793713+cheungkw0@users.noreply.github.com>
2022-04-11 00:02:01 +0800

    Questionnaire update

--- frontend/src/pages/Questionnaire.js
+++ frontend/src/pages/Questionnaire.js
@@ -3,60 +3,93 @@ import './Questionnaire.css'
 import * as Ri from "react-icons/ri";
 import * as Io from "react-icons/io";
 import * as Ai from "react-icons/ai";
-import sample from '../components/sample.jpg'
+import longquestionimg from '../components/LongQ.png'
+import mcimg from '../components/MC.png'
+import scoringimg from '../components/scoring.jpg'
+import googleFormimg from '../components/googleForm.jfif'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function Questionnaire() {
-  return (
-    <div className="questionnaire_page">
-      <header className="questionnaire_header">
-        <div className="questionnaire_icon">
-          <Ri.RiNewspaperLine />
+  const [data, setData] = useState();
+  const [isLoading, setLoading] = useState(true);
+  const [dataArray, setDataArray] = useState();
+
+  function questionnaireSearch(){
+    let x = document.getElementById("questionnaire_search_bar").value
+    x = x.toLowerCase()
+    setDataArray(data.filter((element)=>{
+        return element.title.includes(x);
+    }))
+  }
+
+  useEffect(() => {
+    axios
+        .get("http://localhost:8000/api/questionnaire/ongoing")
+        .then((res)=>{
+          setData(res.data)
+          setDataArray(res.data)
+          setLoading(false)
+        })
+        .catch((err)=>{
+          alert("Loading error")
+        });
+  },[])
+
+  if(isLoading){
+    return (<>Loading</>)
+  }
+
+  if(!isLoading){
+    return (
+      <div className="questionnaire_page">
+        <header className="questionnaire_header">
+          <div className="questionnaire_icon">
+            <Ri.RiNewspaperLine />
+          </div>
+          <div className="questionnaire_title">
+            Questionnaire
+          </div>
+        </header>
+
+        <div className="questionnaire_space">
+          &nbsp;
         </div>
-        <div className="questionnaire_title">
-          Questionnaire
+
+        <div className="questionnaire_search">
+          <div className="questionnaire_search_icon"><Ai.AiOutlineSearch /></div>
+            <input id="questionnaire_search_bar" className="questionnaire_search_bar" type="text" placeholder="Search..." onChange={(e)=>{questionnaireSearch()}}/>
+            <div className="questionnaire_search_edit_icon" onClick={()=>{window.location.pathname='/question/edit';}}><Io.IoMdCreate/></div>
         </div>
-      </header>
 
-      <div className="questionnaire_space">
-        &nbsp;
+        <section>
+          {dataArray.filter((element) => {return element.questiontype = "gf"}).map((element,index) => <QuestionnairePost key={index} id={element.id} title={element.title} type={element.questiontype} closing={element.deadline} description={element.description}/>)}
+        </section>
+
       </div>
-
-      <div className="questionnaire_search">
-        <div className="questionnaire_search_icon"><Ai.AiOutlineSearch /></div>
-          <input className="questionnaire_search_bar" type="text" placeholder="Search..." 
-            onChange={(e)=>{
-              let SearchItemsLowerCase = e.target.value.toLowerCase();
-              setSearchItems(SearchItemsLowerCase);}}/>
-          <div className="questionnaire_search_edit_icon" onClick={()=>{window.location.pathname='/question/edit';}}><Io.IoMdCreate/></div>
-      </div>
-
-      <section>
-        <QuestionnairePost id="1" type="MC"/>
-        <QuestionnairePost id="2" type="google_form"/>
-        <QuestionnairePost id="3" type="Long_question"/>
-        <QuestionnairePost id="4" type="scoring"/>
-        <QuestionnairePost id="5"/>
-      </section>
-
-    </div>
-  )
+    )
+  }
 }
 
 export default Questionnaire
 
 
-function QuestionnairePost({id, type}){
+function QuestionnairePost({id, type, title, description}){
   return(
     <div>
       <div className="questionnaire_post">
           <div className="questionnaire_img">
-            <img src={sample}/>
+            {type == "mc" && <img src={mcimg}/>}
+            {type == "sc" && <img src={scoringimg}/>}
+            {type == "gf" && <img src={googleFormimg}/>}
+            {type == "lq" && <img src={longquestionimg}/>}
           </div>
           <div className="questionnaire_post_content">
             <ul>
-              <li className="questionnaire_post_title">title</li>
-              <li>question type: <b>{type}</b></li>
-              <li>expected finishing time: <b>10 mins</b></li>
+              <li className="questionnaire_post_title">{title}</li>
+              <li>Question type: {type == "mc" && <b>MC</b>} {type == "lq" && <b>Long Question</b>} {type == "gf" && <b>Google Form</b>} {type == "sc" && <b>Scoring</b>}</li>
+              <li><div className="questionnaire_decription">Description: {description}</div></li>
+              <li>Expected finishing time: <b>10 mins</b></li>
               <li><a href={"http://localhost:3000/question/" + id}>More..</a></li>
             </ul>
           </div>
@@ -64,8 +97,4 @@ function QuestionnairePost({id, type}){
         <hr className="questionnaire_post_line"/>
     </div>
   )
-}
-
-function setSearchItems(){
-  
 }
--- frontend/src/pages/Questionnaire_edit.js
+++ frontend/src/pages/Questionnaire_edit.js
@@ -3,9 +3,47 @@ import * as Ri from "react-icons/ri";
 import * as Io from "react-icons/io";
 import * as Im from "react-icons/im";
 import * as Md from "react-icons/md";
-import sample from '../components/sample.jpg'
+import longquestionimg from '../components/LongQ.png'
+import mcimg from '../components/MC.png'
+import scoringimg from '../components/scoring.jpg'
+import googleFormimg from '../components/googleForm.jfif'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function Questionnaire_edit() {
+  const [loadingProfile, setLoadingProfile] = useState(true);
+  const [profile, setProfile] = useState();
+  const [data, setData] = useState();
+  const [loading, isLoading] = useState(true);
+
+  useEffect(()=> {
+        axios
+            .get("http://localhost:8000/api/questionnaire/")
+            .then((res) => {
+                setData(res.data)
+                isLoading(false)
+            })
+            .catch((err) => {
+                alert("Error in loading")
+            });
+
+        axios
+            .get("http://localhost:8000/api/login", {withCredentials: true})
+            .then((res) => {
+                setProfile(res.data)
+                setLoadingProfile(false)
+            })
+            .catch((err) => {
+                alert(err.reponse.data.message)
+            })
+    },[])
+  
+
+  if(loading || loadingProfile){
+      return <>Loading</>;
+  }
+
+  if(!loading && !loadingProfile){
     return (
       <div className="questionnaire_page">
       <header className="questionnaire_header">
@@ -28,36 +66,38 @@ function Questionnaire_edit() {
         </div>
 
         <section>
-          <QuestionnairePost id="1" type="MC"/>
-          <QuestionnairePost id="2" type="google_form"/>
-          <QuestionnairePost id="3" type="Long_question"/>
-          <QuestionnairePost id="4" type="scoring"/>
-          <QuestionnairePost id="5"/>
+            {data.filter((element) => {return element.host.username == profile.username}).map((element,index) => <QuestionnairePost id={element.id} title={element.title} type={element.questiontype} closing={element.deadline} description={element.description}/>)}
         </section>
       </div>
     )
+  }
 }
+
   
 export default Questionnaire_edit;
 
-function QuestionnairePost({id, type}){
+function QuestionnairePost({id, type, title, description}){
     return(
       <div>
         <div className="questionnaire_post">
-            <div className="questionnaire_img">
-              <img src={sample}/>
+            <div className="questionnaire_edit_img">
+            {type == "mc" && <img src={mcimg}/>}
+            {type == "sc" && <img src={scoringimg}/>}
+            {type == "gf" && <img src={googleFormimg}/>}
+            {type == "lq" && <img src={longquestionimg}/>}
             </div>
-            <div className="questionnaire_post_content">
-              <ul>
-                <li className="questionnaire_post_title">title</li>
-                <li>question type: <b>{type}</b></li>
-                <li>expected finishing time: <b>10 mins</b></li>
-              </ul>
-              <div className='questionnaire_button'>
-                <div id='edit'><Io.IoMdCreate/>&nbsp;Edit</div>
-                <div id='delete'><Im.ImBin/>&nbsp;Delete</div>
-                </div>
+          <div className="questionnaire_post_content">
+            <ul>
+              <li className="questionnaire_post_title">{title}</li>
+              <li>Question type: {type == "mc" && <b>MC</b>} {type == "lq" && <b>Long Question</b>} {type == "gf" && <b>Google Form</b>} {type == "sc" && <b>Scoring</b>}</li>
+              <li><div className="questionnaire_decription">Description: {description}</div></li>
+              <li>expected finishing time: <b>10 mins</b></li>
+            </ul>
+            <div className='questionnaire_button'>
+              <div id='edit'><Io.IoMdCreate/>&nbsp;Edit</div>
+              <div id='delete'><Im.ImBin/>&nbsp;Delete</div>
               </div>
+            </div>
           </div>
           <hr className="questionnaire_post_line"/>
       </div>
