87c6e7df3bc446500f4cf700f8d88e17287bb92f
Edmond Kwok <80093963+EdmondKwok@users.noreply.github.com>
2022-04-12 01:04:42 +0800

    Update Exp_add.js

--- frontend/src/pages/Exp_add.js
+++ frontend/src/pages/Exp_add.js
@@ -55,7 +55,7 @@ function List(){
       {matchData.map(item=>(
       <><div className='container1'>
       <ul className='exp_list'>
-          <li className='picture'><img src={item.exp_img} width="300px" height="280px"></img></li>
+          <li className='picture'><img src={item.exp_img} width="88%" height="88%"></img></li>
       <li className='exp_item'>
               <div id='exp_date'>{item.post_date.substring(0,10)}</div>
               <div id='exp_title'>{item.title.substring(0,200)}</div>
