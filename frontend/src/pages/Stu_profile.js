cb6d8c766091141b18b14a98dd91321a8acf39cf
cheungkw0 <79793713+cheungkw0@users.noreply.github.com>
2022-04-14 12:56:04 +0800

    Stu profile update

--- frontend/src/pages/Stu_profile.js
+++ frontend/src/pages/Stu_profile.js
@@ -34,25 +34,25 @@ if(items.gender=="NA"){
       <div className='title1'>Profile</div>
     </div><div style={{ display: "flex" }}>
         <div className='left_card'>
-          <div id="avatar"><img id="avatar1" src={items.avatar} width="300px" height="300px" /></div>
-          <div id="sid"style={{marginTop:"410px"}}>SID:{items.sid}</div>
+          <div id="avatar"><img id="avatar1" src={items.avatar} width="100%" height="100%" /></div>
+          <div id="sid"style={{marginTop:"5vw"}}>SID:{items.sid}</div>
           <div id="major"style={{marginTop:"0px"}}>Major: {items.major}</div>
           <div id="admission"style={{marginTop:"0px"}}>Admission Year: {items.admission_year}</div>
         </div>
         <div className='right_card'>
-          <div id="fullname">Username: <div style={{ marginLeft: "52px", fontWeight: "lighter" }}>{items.user.username}</div></div>
+          <div id="fullname">Username: <div style={{ marginLeft: "2.184vw", fontWeight: "lighter" }}>{items.user.username}</div></div>
           <hr className='profile_line' />
-          <div id="email">Email: <div style={{ marginLeft: "113px", fontWeight: "lighter" }}>{items.user.email}</div></div>
+          <div id="email">Email: <div style={{ marginLeft: "4.746vw", fontWeight: "lighter" }}>{items.user.email}</div></div>
           <hr className='profile_line' />
-          <div id="birth">Date of Birth: <div style={{ marginLeft: "20px", fontWeight: "lighter" }}>{items.date_of_birth}</div></div>
+          <div id="birth">Date of Birth: <div style={{ marginLeft: "0.84vw", fontWeight: "lighter" }}>{items.date_of_birth}</div></div>
           <hr className='profile_line' />
-          <div id="gender">Gender:<div style={{ marginLeft: "93px", fontWeight: "lighter" }}> {gender}</div></div>
+          <div id="gender">Gender:<div style={{ marginLeft: "3.906vw", fontWeight: "lighter" }}> {gender}</div></div>
         </div>
       </div>
-      {feature == "exp" && <button className='back_exp' style={{ marginLeft: "1800px" }} onClick={()=>{window.location.pathname='/experiments/add/response'+id;}}>Back</button>}
-      {feature == "team" && <button className='back_exp' style={{ marginLeft: "1800px" }} onClick={()=>{window.location.pathname='/team/'+id;}}>Back</button>}
-      {feature == "team_edit" && <button className='back_exp' style={{ marginLeft: "1800px" }} onClick={()=>{window.location.pathname='/team/response/'+id;}}>Back</button>}
-      {feature == "questionnaire" && <button className='back_exp' style={{ marginLeft: "1800px" }} onClick={()=>{window.location.pathname='/questionnaire/response/'+id;}}>Back</button>}
+      {feature == "exp" && <button className='back_exp' style={{ marginLeft: "75.6vw" }} onClick={()=>{window.location.pathname='/experiments/add/response'+id;}}>Back</button>}
+      {feature == "team" && <button className='back_exp' style={{ marginLeft: "75.6vw" }} onClick={()=>{window.location.pathname='/team/'+id;}}>Back</button>}
+      {feature == "team_edit" && <button className='back_exp' style={{ marginLeft: "75.6vw" }} onClick={()=>{window.location.pathname='/team/response/'+id;}}>Back</button>}
+      {feature == "questionnaire" && <button className='back_exp' style={{ marginLeft: "75.6vw" }} onClick={()=>{window.location.pathname='/questionnaire/response/'+id;}}>Back</button>}
       </>
   )}catch(e){
     return("")
